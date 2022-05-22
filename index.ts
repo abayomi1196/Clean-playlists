af2b62a461604e6a050706f06b3e73ac11e7db39
Abayomi Ayodele <samabayomi301@gmail.com>
2022-05-22 19:32:09 +0100

    Update

--- index.ts
+++ index.ts
@@ -11,12 +11,12 @@ const SpotifyStrategy = require("passport-spotify").Strategy;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-let REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:8888";
+let REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:8888/callback";
 let FRONTEND_URI = process.env.FRONTEND_URI || "http://localhost:3000";
 const PORT = process.env.PORT || 8888;
 
 if (process.env.NODE_ENV !== "production") {
-  REDIRECT_URI = "http://localhost:8888";
+  REDIRECT_URI = "http://localhost:8888/callback";
   FRONTEND_URI = "http://localhost:3000";
 }
 
@@ -36,7 +36,7 @@ passport.use(
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       //remember to add this to your developer dashboard redirect uri
-      callbackURL: `${REDIRECT_URI}/auth/spotify/callback`
+      callbackURL: `${REDIRECT_URI}`
     },
     function (accessToken, refreshToken, expires_in, profile, done) {
       return done(null, profile, {
@@ -107,7 +107,7 @@ app.get(
 );
 
 app.get(
-  "/auth/spotify/callback",
+  "/callback",
   passport.authenticate("spotify", {
     failureRedirect: REDIRECT_URI
   }),
